eb59a5dde11a3abdccd0c44e9ba8b56c9d50c4f1
Hayden Williford <123603483+hwillif@users.noreply.github.com>
2025-11-12 19:30:20 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "hwill"
+  default     = "hwilli"
 }
 
 variable "class_name" {
