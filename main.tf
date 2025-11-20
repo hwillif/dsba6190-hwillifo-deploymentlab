77c3b5aae297c168de3968a4918d67f98ba5f915
Hayden Williford <123603483+hwillif@users.noreply.github.com>
2025-11-19 19:58:19 -0500

    Fixed IP address to dummy

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "azurerm_storage_account" "storage" {
 
   network_rules {
     default_action             = "Deny"
-    ip_rules                   = ["192.168.86.133"]
+    ip_rules                   = ["100.0.0.1"]
     virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
   }
 
